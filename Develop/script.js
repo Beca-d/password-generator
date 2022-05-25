a2aec7f6793ff31aa28b4fc1c7ec99cf13335b3e
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-05-25 03:07:04 -0400

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -10,8 +10,37 @@
 // ** perhaps using index ranges?
 // show password in window alert 
 
+/*var pCriteria = function() {
 
-// Assignment code here
+  var passLength = window.prompt("How long is your password? (enter a number between 8 and 128)");
+  if (passLength < 8 || passLength > 128) {
+    window.alert("You choose an invalid password length! Please try again.");
+    return passwordCriteria();
+  }
+}
+
+passwordCriteria()
+  
+  var useUppCase = window.confirm("Will your password contain UPPERCASE letters?");
+  var useLowCase = window.confirm("Will your password contain lowercase letters?");
+  var useNum = window.confirm("Will your password contain numbers?");
+  var useSpecial = window.confirm("Will your password contain special characters?");
+*/
+// function to check if player wants to fight or skip
+var fightOrSkip = function() {
+  // ask player if they'd like to fight or run
+  var promptFight = window.prompt('How long is your password? (Enter a number between 8 and 128)');
+
+  // validate prompt answer
+  if (promptFight < 8 || promptFight > 128) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    // use return to call it again and stop the rest of this function from running
+    return fightOrSkip();
+  }
+}
+
+fightOrSkip()
+console.log(promptFight)
 
 
 // Get references to the #generate element
@@ -24,11 +53,26 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+}10
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// copied code
 
+/*function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
-console.log(Math)
+var i = 0;
+while(i<20)
+{
+$('#pwcontainer').html( $('#pwcontainer').html()+'<br>'+generatePassword() );
+i++;
+}*/
