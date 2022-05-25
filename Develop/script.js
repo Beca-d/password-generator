8383b01d503890daa08995815fa4248c46bcc5a9
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-05-25 01:31:11 -0400

    wrote pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,16 @@
+// Generate a password
+// Prompt that allows user to select character length between 8 and 128
+// *user input dictates number of generated characters from Array
+// * if less that 8 and more than 128 alert says picked invalid length and asks again
+// User selects criteria
+// *4 window prompts for lowercase, uppercase, special character, and number
+// confirm selection with prompt
+// use else if statement to run password generator with correct symbols (16) total 
+// * figure out how to have array with toggle for some characters on or off
+// ** perhaps using index ranges?
+// show password in window alert 
+
+
 // Assignment code here
 
 
@@ -15,3 +28,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+console.log(Math)
